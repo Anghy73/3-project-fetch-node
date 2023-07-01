86b61ee027b91ce9e89525f16f5767f7bbd89636
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2023-06-30 21:51:45 -0400

    fetch data ready

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const morga = require('morgan')
+const ejs = require('ejs')
+const path = require('path')
 
 const app = express();
 
+app.use(morga('dev'))
+
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+
+const fetchData = async (req, res) => {
+  try {
+    const response = await fetch('http://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    res.render('home', {
+      data
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  fetchData(req, res)
+  // res.send('Hola Mundo <3')
 })
 
-app.listen(7000)
+app.listen(7000, () => {
+  console.log('Server ready in the port 7000')
+})
