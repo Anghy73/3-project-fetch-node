ad4e5eee08227872b94f29b7f52281827faeae8d
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2023-07-01 23:01:09 -0400

    implemented some functions

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const path = require('path')
 
 const app = express();
 
-
 app.set('SERVER', process.env.SERVER || 'Server-Fetch')
 app.set('PORT', process.env.PORT || 7000)
 app.set('API', process.env.API)
@@ -15,12 +14,15 @@ app.set('view engine', 'ejs')
 
 app.use(morga('dev'))
 
-const fetchData = async (req, res) => {
+let count = 0
+
+const fetchData = async (req, res, page) => {
   try {
     const response = await fetch(app.get('API'))
     const data = await response.json()
-    res.render('home', {
-      data
+    res.render(page, {
+      data,
+      count
     })
   } catch (e) {
     console.log(e)
@@ -28,8 +30,20 @@ const fetchData = async (req, res) => {
 }
 
 app.get('/', (req, res) => {
-  fetchData(req, res)
+  fetchData(req, res, 'home')
 })
+
+app.get('/blogs', (req, res) => {
+  fetchData(req, res, 'blogs')
+})
+
+// app.get('/blogs/:id', (req, res) => {
+//   fetchData(req, res, 'blogs')
+// })
+
+
+app.use('/static', express.static(path.join(__dirname, '/static')))
+
 
 app.listen(app.get('PORT'), () => {
   console.log(`${app.get('SERVER')} ready on the port ${app.get('PORT')}`)
